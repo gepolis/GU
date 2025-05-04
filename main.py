c1157647178fda070021b24dd1164fb067ddd531
gepolis <120267018+gepolis@users.noreply.github.com>
2025-05-05 01:32:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ def setup():
 def id_doc():
     return send_from_directory('templates', 'pass_data.htm')
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
