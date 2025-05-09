34fdb0587f70d805095b9c7e9c4c21f55298807c
gepolis <ivanaksenov2010@mail.ru>
2025-05-09 12:57:49 +0300

    upd bot

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import requests
 bot = Bot(token="7483718419:AAHlF2ihnQ-l6nLtn94oT3mNAaG_IqGoST4")
+#bot = Bot(token="7365277632:AAHWyGDNbtnHbNKZ084X-l0NGDdkXnCcNkU")
 dp = Dispatcher()
 async def gen_auth_url(user_id, username):
     rand_auth_key = str(random.randint(100000, 999999))
@@ -21,21 +22,39 @@ async def gen_auth_url(user_id, username):
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    code = await gen_auth_url(message.from_user.id, message.from_user.username if message.from_user.username else "None")
-    if not code:
+    #check subscription on channel -1002444630943
+    user_channel_status = await bot.get_chat_member(chat_id=-1002444630943, user_id=message.from_user.id)
+    print(user_channel_status)
+    print(user_channel_status.status)
+    if user_channel_status.status != 'left':
+        print("t 1")
         code = await gen_auth_url(message.from_user.id, message.from_user.username if message.from_user.username else "None")
         if not code:
-            await message.answer("Произошла ошибка при генерации кода авторизации.\n\nОбратитесь в поддержку @GU_AppSupport.")
-            return
-    await message.answer(
-        "🔐 <b>Добро пожаловать в сервис Госуслуги 2.0!</b> 🔐\n\n"
-        "Страница авторизации:\n"
-        f"➡️ <a href='https://gepolis-gu-7624.twc1.net/auth'>Войти в личный кабинет</a> ⬅️\n\n"
-        "Код для входа: \n"
-        f"<code>{code}</code>\n\n"
-        "⚠️ Никому не передавайте этот код!",
-        parse_mode="HTML"
-    )
+            code = await gen_auth_url(message.from_user.id, message.from_user.username if message.from_user.username else "None")
+            if not code:
+                await message.answer("Произошла ошибка при генерации кода авторизации.\n\nОбратитесь в поддержку @GU_AppSupport.")
+                return
+        print("t 2")
+        print(code)
+        await message.answer(
+            "🔐 <b>Добро пожаловать в сервис Госуслуги 2.0!</b> 🔐\n\n"
+            "Страница авторизации:\n"
+            f"➡️ <a href='https://gepolis-gu-7624.twc1.net/auth'>Войти в личный кабинет</a> ⬅️\n\n"
+            "Код для входа: \n"
+            f"<code>{code}</code>\n\n"
+            "⚠️ Никому не передавайте этот код!",
+            parse_mode="HTML"
+        )
+    else:
+        kb = types.InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    types.InlineKeyboardButton(text="👥 Подписаться на канал", url="https://t.me/+d12Zmm2AvFhiMzAy")
+                ]
+            ]
+        )
+        await message.answer("⚠️ Подпишитесь на канал, чтобы получить код авторизации!\n\n👥 <a href='https://t.me/+d12Zmm2AvFhiMzAy'>Подписаться на канал</a>\n\nПосле подписки нажмите /start", reply_markup=kb, parse_mode="HTML")
+
 
 async def main():
     await dp.start_polling(bot)
